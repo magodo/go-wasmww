c3e23e2de77d79f144f4905559838ca52b2c4602
magodo <wztdyl@sina.com>
2023-09-10 20:57:35 +0800

    Example: test more

--- examples/shared/control/main.go
+++ examples/shared/control/main.go
@@ -18,15 +18,14 @@ import (
 
 func main() {
 	os.Setenv("parent_foo", "parent_bar")
-	conn := &wasmww.WasmSharedWebWorkerConn{
+	conn1 := &wasmww.WasmSharedWebWorkerConn{
 		Name: "hello",
 		Path: "hello.wasm",
 	}
-	consoleConn, err := conn.Start()
+	consoleConn, err := conn1.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var stdout, stderr bytes.Buffer
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -41,26 +40,76 @@ func main() {
 	}()
 	wg.Add(1)
 	go func() {
-		handle(conn.EventChannel())()
+		handle(conn1.EventChannel())()
 		wg.Done()
 	}()
-	if err := conn.PostMessage(safejs.Safe(js.ValueOf("Hello Foo!")), nil); err != nil {
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("Hello Foo!")), nil); err != nil {
 		log.Fatal(err)
 	}
-	if err := conn.PostMessage(safejs.Safe(js.ValueOf("Hello Bar!")), nil); err != nil {
-		log.Fatal(err)
-	}
-	if err := conn.PostMessage(safejs.Safe(js.ValueOf("Hello Baz!")), nil); err != nil {
-		log.Fatal(err)
-	}
-	if err := conn.PostMessage(safejs.Safe(js.ValueOf("ClosePort")), nil); err != nil {
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("Hello Bar!")), nil); err != nil {
 		log.Fatal(err)
 	}
 
-	// Wait for the 1st port closed
-	conn.Wait()
+	// Create a 2nd connection to the shared worker
+	conn2 := &wasmww.WasmSharedWebWorkerConn{
+		Name: conn1.Name,
+		URL:  conn1.URL,
+	}
+	if err := conn2.Connect(); err != nil {
+		log.Fatal(err)
+	}
 
-	// Wait for the worker closed, which closes the console connection
+	// Close and wait for the 1st port closed when the 2nd connection is active, to avoid the worker to close itself.
+	// Since on the worker side, we will close it if there is no active connection.
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("ClosePort")), nil); err != nil {
+		log.Fatal(err)
+	}
+	conn1.Wait()
+
+	wg.Add(1)
+	go func() {
+		handle(conn2.EventChannel())()
+		wg.Done()
+	}()
+
+	if err := conn2.PostMessage(safejs.Safe(js.ValueOf("Hi Foo!")), nil); err != nil {
+		log.Fatal(err)
+	}
+	if err := conn2.PostMessage(safejs.Safe(js.ValueOf("Hi Bar!")), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect conn1 back (to test a closed connection can connect again)
+	if err := conn1.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Add(1)
+	go func() {
+		handle(conn1.EventChannel())()
+		wg.Done()
+	}()
+
+	// Close and wait for the 2nd port closed, after the conn1 is connected again
+	if err := conn2.PostMessage(safejs.Safe(js.ValueOf("ClosePort")), nil); err != nil {
+		log.Fatal(err)
+	}
+	conn2.Wait()
+
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("Hey Foo!")), nil); err != nil {
+		log.Fatal(err)
+	}
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("Hey Bar!")), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close and wait for the 1st port closed again, this will close the worker as it is the last connection
+	if err := conn1.PostMessage(safejs.Safe(js.ValueOf("ClosePort")), nil); err != nil {
+		log.Fatal(err)
+	}
+	conn1.Wait()
+
+	// The close of the worker will close the console connection
 	consoleConn.Wait()
 
 	// Wait until the console output streams are all copied and event handler done
